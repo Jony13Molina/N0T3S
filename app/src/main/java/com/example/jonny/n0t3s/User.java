914c388c0a809a69084bbc5e58e8cb8c9b30a5dd
JonnyMoli95 <molinacjony@gmail.com>
2019-03-26 22:09:27 -0700

    login

--- app/src/main/java/com/example/jonny/n0t3s/User.java
+++ app/src/main/java/com/example/jonny/n0t3s/User.java
@@ -1,4 +1,69 @@
 package com.example.jonny.n0t3s;
 
-public class User {
+import java.util.Date;
+import java.util.Calendar;
+
+/**
+ * Created by jonny on 2/15/2018.
+ */
+
+public class User{
+    //global variables for helper class
+    public String userID;
+    public String name;
+    public Date currLogin;
+    public String userEmail;
+
+
+    //constructors
+    public User(String myId, String name, Date currLogin, String email)
+    {
+        this.userID = myId;
+        this.name = name;
+        this.currLogin = currLogin;
+        this.userEmail = email;
+
+    }
+
+    public User(String userid)
+    {
+        this.userID = userid;
+
+    }
+    public User()
+    {
+
+    }
+    //setters
+    public void setUserID(String id)
+    {
+        this.userID = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setUserEmail(String email)
+    {
+        this.userEmail = email;
+    }
+
+
+    //getters
+    public String getUserID()
+    {
+        return this.userID;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public String getUserEmail()
+    {
+        return this.userEmail;
+    }
+    public String getCurrLogin()
+    {
+        return Calendar.getInstance().getTime().toString();
+    }
+
+
 }
