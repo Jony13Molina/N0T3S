f7a5956c3831a35e5dd53c64a1d30041013b7066
JonnyMoli95 <molinacjony@gmail.com>
2019-07-20 14:56:05 -0700

    mvp viewInfo

---
+++ app/src/main/java/com/example/jonny/n0t3s/viewInteractorImp.java
@@ -0,0 +1,4 @@
+package com.example.jonny.n0t3s;
+
+public class viewInteractorImp implements viewInfoInteractor {
+}
---
+++ app/src/main/java/com/example/jonny/n0t3s/viewPresentImp.java
@@ -0,0 +1,4 @@
+package com.example.jonny.n0t3s;
+
+public class viewPresentImp implements viewPresenter {
+}
