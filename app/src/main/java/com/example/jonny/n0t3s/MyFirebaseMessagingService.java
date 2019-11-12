a8c94db906617504f1dd5f0e23d2e10b5c78635f
Jonathan Molina Cobos <jompyjompmuaythai@jonathans-imac.attlocal.net>
2019-11-11 16:53:17 -0800

    changes

---
+++ app/src/main/java/com/example/jonny/n0t3s/MyFirebaseMessagingService.java
@@ -0,0 +1,14 @@
+package com.example.jonny.n0t3s;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    public MyFirebaseMessagingService() {
+    }
+
+
+}
